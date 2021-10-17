d5d7ba25e06ae2cd9704bd3a896dcf6172007d2a
luizfboss <luiz.fbossetto@outlook.com>
2021-10-17 11:16:54 -0300

    File Upload
    File Upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var date = new Date()
+    var hour = date.getHours()
+    msg.innerHTML = `It is ${hour} o'clock.`
+    if (hour >= 0 && hour < 12){
+        img.src = 'Images/morning.png'
+        document.body.style.background = '#e2cd9f'
+    } else if(hour >= 12 && hour <= 18){
+        img.src = 'Images/afternoon.png'
+        document.body.style.background = '#b98446f'
+    } else {
+        img.src = 'Images/night.png'
+        document.body.style.background = '#515154'
+    }
+}
